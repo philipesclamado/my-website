aeb8b7a1657be1ea8e2d38c3cb483da8f61e1a7d
Philip Esclamado <philipesclamado@uvic.ca>
2022-11-05 14:14:05 -0700

    chore: update pdf links

--- src/constants.ts
+++ src/constants.ts
@@ -10,12 +10,12 @@ export const Articles = [
     title:
       "Velocity measurement of a ball in motion via intensity-based kinematics",
     date: "2021-03-26",
-    link: "https://cvws.icloud-content.com/B/AUMbjI4Kkwu2-WbQMzkRhGyTXKnmAc9OdhxO-DaM6LDVFySQQncJP_D6/Velocity+measurement+of+a+ball+in+motion+via+intensity.pdf?o=AiTuhu8mW7iB1ygOojsXh_WY3yECp7bsTEI_YfzhfiwL&v=1&x=3&a=CAogGRyHxPOYfYU3DbWNIaRBiwBbEzOjD2WSmp3H897dp3USbxDOgPrKxDAYzt3VzMQwIgEAUgSTXKnmWgQJP_D6aicambSKYuJNcf2k9mCE6JRHvj_72R3TwXfxMzEL3a0Fa-9QzAk9LyhyJ7qDdFdsbkN_d-zOIf7xAkCDOWWXhjhyJw84hTbQzILNsOvLx8ugCA&e=1667681840&fl=&r=6f83ebbc-84b6-48da-b7bc-80fbda510769-1&k=6wSGrfxnia3ilIE39FcD_A&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=139&s=WxkqBt3sQs5g2-Ne_bX9oK5IgMg&cd=i",
+    link: "https://uvic-my.sharepoint.com/:b:/r/personal/philipesclamado_uvic_ca/Documents/Velocity%20measurement%20of%20a%20ball%20in%20motion%20via%20intensity.pdf?csf=1&web=1&e=Ia5AdO",
   },
   {
     title: "The effects of basicity in exhibiting chemiluminescence of Luminol",
     date: "2021-03-16",
-    link: "https://cvws.icloud-content.com/B/Af2a7LmvmuNGbNOaDj05D1A7aN0lAW5VKRZ4D5ECZuCfguJ9gKQuqk7U/The+effects+of+Sodium+Hydroxide+concentration+in+exhibiting+chemiluminescence+of+Luminol.pdf?o=ApqvfRHDtOnUA4FGqLhVcBEWG2HpdkRBDW_amI4-6Zp3&v=1&x=3&a=CAogcbZ08EOSGbb-KeCTFlW1D67OfudXyNPziLrN104QrIoSbxDQpq3LxDAY0IOJzcQwIgEAUgQ7aN0lWgQuqk7Uaie9VBJZa5MEMERy_A8Y1bBFi6IVz95IAV6p38C8YvpJXUE9IBTxl1FyJ_A7L05GpMMwwuzxPu4f2QfHXanL9sd-81MtmeIBqzusvFWRptx2UQ&e=1667682681&fl=&r=d6d08160-abd7-4f1e-99e2-1ef2949a7684-1&k=9D1C-Npv4CBWMixh93Uw6g&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=139&s=GOhzbdZanYNT6UMJ3OEcY2bo_T8&cd=i",
+    link: "https://uvic-my.sharepoint.com/:b:/r/personal/philipesclamado_uvic_ca/Documents/The%20effects%20of%20Sodium%20Hydroxide%20concentration%20in%20exhibiting%20chemiluminescence%20of%20Luminol.pdf?csf=1&web=1&e=IOm4r5",
   },
 ];
 
