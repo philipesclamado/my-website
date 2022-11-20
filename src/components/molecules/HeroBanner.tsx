2038b8c0f73180af82faecb38d3fc87c6d6351a1
Philip <philipesclamado@uvic.ca>
2022-11-20 13:56:25 -0800

    feat: hero banner

---
+++ src/components/molecules/HeroBanner.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import { FaEnvelope, FaGithub } from "react-icons/fa";
+import { CachedThoughts } from "../../constants";
+
+export const HeroBanner = () => {
+  return (
+    <div id="about">
+      <p className="mt-4 text-base md:text-sm">Some cached thoughts:</p>
+      {CachedThoughts.map((thoughts, i) => {
+        return (
+          <p className="text-base md:text-sm" key={i}>
+            {thoughts}
+          </p>
+        );
+      })}
+      <p className="flex flex-row my-4 text-base md:text-sm">
+        Find me on&nbsp;
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/philipesclamado"
+        >
+          <FaGithub className="h-5 w-5 fill-current hover:text-highlightColor hover:cursor-pointer" />
+        </Link>
+        ,&nbsp;
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="mailto:philipesclamado@uvic.ca"
+        >
+          <FaEnvelope className="h-5 w-5 fill-current hover:text-highlightColor hover:cursor-pointer" />
+        </Link>
+        &nbsp;.
+      </p>
+    </div>
+  );
+};
