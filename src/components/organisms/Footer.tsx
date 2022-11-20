bdb312c4d606cb37e5fe3c8d8601e740d734580a
Philip Esclamado <philipesclamado@uvic.ca>
2022-11-20 14:02:05 -0800

    fix: footer nav margin

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <footer className="flex justify-between text-xs mt-14">
       <div>Copyright &copy; 2022 Philip Esclamado</div>
-      <NavBar className="ml-20 underline space-x-7" />
+      <NavBar className="ml-4 underline space-x-7" />
     </footer>
   );
 };
