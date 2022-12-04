243f86bc47022775d1444e40e064be3d48a50fb0
Philip Esclamado <philipesclamado@uvic.ca>
2022-12-04 14:38:31 -0800

    fix: unused nav prop

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -5,17 +5,11 @@ interface NavBarProps {
   navStyle?: string;
   borderStyle?: string;
   hoverStyle?: string;
-  className?: string;
 }
 
-export const NavBar = ({
-  navStyle,
-  borderStyle,
-  hoverStyle,
-  className,
-}: NavBarProps) => {
+export const NavBar = ({ navStyle, borderStyle, hoverStyle }: NavBarProps) => {
   return (
-    <div className={classNames(className)} id="nav">
+    <div id="nav">
       <ul className={classNames(navStyle, "flex", "flex-row")}>
         <li>
           <NavLink name="Home" link="/" className={classNames(hoverStyle)} />
