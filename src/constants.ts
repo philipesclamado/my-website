ff8218fa98726e7f448741963ba77d213b093f46
Philip Esclamado <philipesclamado@uvic.ca>
2023-01-10 14:07:10 -0800

    chore: update projects section

--- src/constants.ts
+++ src/constants.ts
@@ -13,19 +13,7 @@ export const Projects = [
       "an Unmanned Surface Vehicle that filter microplastics in the ocean using microbubble as method of filtration",
     link: "https://github.com/UVic-Environmental-Engineering-Club/USV-Pi/wiki/Project-Structure",
   },
-  {
-    title: "Doppler Velocity Log",
-    description:
-      "an acoustic sensor that estimates velocity relative to the sea bottom",
-    link: "https://github.com/philipesclamado/doppler-velocity-log",
-  },
-  {
-    title: "Inertial Measurement Unit",
-    description:
-      "an electronic device that measures and reports a body's specific force, angular rate, and sometimes the orientation of the body",
-    link: "https://github.com/philipesclamado/vertical-reference-unit",
-  },
-];
+  ];
 
 export const NavMap = [
   { name: "Home", link: "/" },
