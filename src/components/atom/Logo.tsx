c01d480ebe12ac72125475859920e132a34be0bf
Philip <philipesclamado@uvic.ca>
2023-01-29 22:01:20 -0800

    chore: unnecessary code

--- src/components/atom/Logo.tsx
+++ src/components/atom/Logo.tsx
@@ -2,21 +2,19 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <div id="logo">
+    <div>
       <Image
         className="opcaity-100 absolute rounded-full"
         height={60}
         width={60}
-        src="/images/mugshot-mono.jpg"
-        id="logo"
+        src="/images/id-mono.jpg"
         alt={""}
       />
       <Image
         className="absolute rounded-full opacity-0 hover:opacity-100 hover:cursor-pointer"
         height={60}
         width={60}
-        src="/images/mugshot.jpg"
-        id="logo"
+        src="/images/id.jpg"
         alt={""}
       />
     </div>
