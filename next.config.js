0996412128c669b890cf0edfe4376c4a7073cc9d
Philip <philipesclamado@uvic.ca>
2023-01-29 22:01:46 -0800

    fix: next config warning

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = {
-  nextConfig,
   images: {
     domains: ["media.graphassets.com"],
   },
 };
+
+module.exports = nextConfig;
