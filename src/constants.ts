c0d9eafe9da7aff2c503549861d435d50d05f06c
Philip <philipesclamado@uvic.ca>
2023-02-05 10:22:55 -0800

    chore: add project tag

--- src/constants.ts
+++ src/constants.ts
@@ -8,12 +8,12 @@ export const CachedThoughts = [
 
 export const Projects = [
   {
-    title: "USV",
+    title: "USV (environmental engineering)",
     description:
       "an Unmanned Surface Vehicle that filter microplastics in the ocean using microbubble as method of filtration",
     link: "https://github.com/UVic-Environmental-Engineering-Club/USV-Pi/wiki/Project-Structure",
   },
-  ];
+];
 
 export const NavMap = [
   { name: "Home", link: "/" },
