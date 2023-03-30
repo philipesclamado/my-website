7040ad018d39a5c78b8ec7dbb91533dafbe12314
Philip <philipesclamado@uvic.ca>
2023-03-30 13:31:52 -0700

    chore: added new project

--- src/constants.ts
+++ src/constants.ts
@@ -13,6 +13,12 @@ export const Projects = [
       "an Unmanned Surface Vehicle that filter microplastics in the ocean using microbubble as method of filtration",
     link: "https://github.com/UVic-Environmental-Engineering-Club/USV-Pi/wiki/Project-Structure",
   },
+  {
+    title: "Hybrid-Propellant Rocket Engine Modelling (aerospace)",
+    description:
+      "a hybrid-propellant rocket engine that uses a combination of liquid and solid propellants",
+    link: "https://github.com/philipesclamado/hybrid-modelling",
+  },
 ];
 
 export const NavMap = [
