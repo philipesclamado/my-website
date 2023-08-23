c34276ec36cc89568b8773e4d7ad5006d492bd9a
Philip <philipesclamado@uvic.ca>
2023-08-23 14:44:49 -0600

    feat: live year display

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { NavBar } from "../molecules";
+import { Brand } from "../../constants";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="flex flex-col md:flex-row justify-between items-center text-xs mt-14 space-y-2 md:space-y-0">
-      <div>Copyright &copy; 2022 Philip Esclamado</div>
+      <div>
+        Copyright &copy; {currentYear} {Brand}
+      </div>
       <NavBar
         navStyle="ml-4 underline space-x-6"
         borderStyle="border-[0.5px] border-solid border-black"
