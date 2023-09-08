df6c641bb765a0480e8dcd930c39f83d70103b7d
Philip Esclamado <philipesclamado@uvic.ca>
2023-09-08 00:58:21 -0700

    chore: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -13,16 +13,4 @@ export const Projects = [
       "an Unmanned Surface Vehicle that filter microplastics in the ocean using microbubble as method of filtration",
     link: "https://github.com/UVic-Environmental-Engineering-Club/USV-Pi/wiki/Project-Structure",
   },
-  {
-    title: "Hybrid-Propellant Rocket Engine Modelling (aerospace engineering)",
-    description:
-      "a hybrid-propellant rocket engine that uses a combination of liquid and solid propellants",
-    link: "https://github.com/philipesclamado/hybrid-modelling",
-  },
-  {
-    title: "Underwater Glider Prototype-1 (marine engineering)",
-    description:
-      "a prototype of an underwater glider that uses buoyancy to propel itself",
-    link: "/files/Prototype_1_Vehicle_Systems.pdf",
-  },
 ];
