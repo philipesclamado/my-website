53bb4c7df93828948e8d4abf152c2d92b21f5542
Philip Esclamado <philipesclamado@uvic.ca>
2026-09-11 01:00:17 -0600

    fix: format dates as YYYY-MM-DD

--- app/elements/utils.ts
+++ app/elements/utils.ts
@@ -61,15 +61,15 @@ export function getBlogPosts() {
 }
 
 export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date();
+  const currentDate = new Date();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date);
+  const targetDate = new Date(date);
 
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-  let daysAgo = currentDate.getDate() - targetDate.getDate();
+  const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo = currentDate.getDate() - targetDate.getDate();
 
   let formattedDate = "";
 
@@ -83,11 +83,11 @@ export function formatDate(date: string, includeRelative = false) {
     formattedDate = "Today";
   }
 
-  let fullDate = targetDate.toLocaleString("en-us", {
-    month: "long",
-    day: "numeric",
+  const fullDate = new Intl.DateTimeFormat("en-CA", {
     year: "numeric",
-  });
+    month: "2-digit",
+    day: "2-digit",
+  }).format(targetDate);
 
   if (!includeRelative) {
     return fullDate;
