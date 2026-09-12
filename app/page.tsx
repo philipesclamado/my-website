1269b01fc705d570ae1bc861535d286cef3f94f4
Philip Esclamado <philipesclamado@uvic.ca>
2026-09-12 10:43:52 -0600

    feat: POW button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
 // See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
 import Link from "next/link";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope, FaSnowflake } from "react-icons/fa";
 import Header from "./components/header";
 import { CachedThoughts } from "./constants";
 import { BlogPosts } from "./components/posts";
@@ -21,6 +21,14 @@ export default function Page() {
         <Link
           target="_blank"
           rel="noopener noreferrer"
+          href="https://www.protectourwinters.ca/edmonton_chapter"
+        >
+          <FaSnowflake className="h-5 w-5 fill-current hover:text-highlight hover:cursor-pointer" />
+        </Link>
+        ,&nbsp;
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
           href="https://github.com/philipesclamado"
         >
           <FaGithub className="h-5 w-5 fill-current hover:text-highlight hover:cursor-pointer" />
@@ -37,7 +45,7 @@ export default function Page() {
         <Link
           target="_blank"
           rel="noopener noreferrer"
-          href="mailto:philipesclamado@uvic.ca"
+          href="mailto:philip.esclamado06@gmail.com"
         >
           <FaEnvelope className="h-5 w-5 fill-current hover:text-highlight hover:cursor-pointer" />
         </Link>
@@ -45,7 +53,7 @@ export default function Page() {
       </p>
       <h1 className="text-xl font-semibold mt-12 mb-4">Elements</h1>
       <BlogPosts />
-      <h1 className="text-xl font-semibold mt-12 mb-4">Projects</h1>
+      <h1 className="text-xl font-semibold mt-12 mb-4">Practice</h1>d
     </section>
   );
 }
